6d0ec7604c944a14ef85fa2b3b869b54988d9ab7
Rajat Khandelwal <rajatgupta59@gmail.com>
2013-03-13 03:23:46 +0530

    added new android app

---
+++ android_new/src/hisaab/hisaab/ItemListActivity.java
@@ -0,0 +1,66 @@
+package hisaab.hisaab;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Gravity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class ItemListActivity extends FragmentActivity implements
+		ItemListFragment.Callbacks {
+
+	private boolean mTwoPane;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_item_list);
+
+		if (findViewById(R.id.item_detail_container) != null) {
+			mTwoPane = true;
+			((ItemListFragment) getSupportFragmentManager().findFragmentById(
+					R.id.item_list)).setActivateOnItemClick(true);
+		}
+		final CharSequence[] items = { "Red", "Green", "Blue" };
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle("Pick a color");
+		builder.setItems(items, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int item) {
+				Toast.makeText(getApplicationContext(), items[item],
+						Toast.LENGTH_SHORT).show();
+			}
+		});
+		setContentView(R.layout.new_layout);
+		
+		AlertDialog alert = builder.create();
+		alert.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	     WindowManager.LayoutParams wmlp = alert.getWindow().getAttributes();
+
+	 wmlp.gravity = Gravity.TOP | Gravity.LEFT;
+	 wmlp.x = 00;   //x position
+	 wmlp.y = 00;   //y position
+	 alert.show();
+	}
+
+	@Override
+	public void onItemSelected(String id) {
+		if (mTwoPane) {
+			Bundle arguments = new Bundle();
+			arguments.putString(ItemDetailFragment.ARG_ITEM_ID, id);
+			ItemDetailFragment fragment = new ItemDetailFragment();
+			fragment.setArguments(arguments);
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.item_detail_container, fragment).commit();
+
+		} else {
+			Intent detailIntent = new Intent(this, ItemDetailActivity.class);
+			detailIntent.putExtra(ItemDetailFragment.ARG_ITEM_ID, id);
+			startActivity(detailIntent);
+		}
+	}
+}
