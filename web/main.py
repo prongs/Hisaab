991059589981548ba66f725906818bf7a2d5ecad
Rajat Khandelwal <rajatgupta59@gmail.com>
2013-03-20 02:21:40 +0530

    removed syncdb for now

--- web/main.py
+++ web/main.py
@@ -65,8 +65,8 @@ class MainHandler(tornado.web.RequestHandler):
 
 # RAMMING SPEEEEEEED!
 def main():
-    print settings.DATABASES
-    execute_from_command_line(["syncdb", "syncdb"])
+    # print settings.DATABASES
+    # execute_from_command_line(["syncdb", "syncdb"])
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(os.environ.get("PORT", 8888))
